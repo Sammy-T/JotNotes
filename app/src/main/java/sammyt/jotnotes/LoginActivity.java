5186ecc8c4d6fd7061ad995f8d2d1591ed951612
Sammy-T <cyam9019@aim.com>
2019-08-01 14:39:10 -0400

    Migrated from Support Library to AndroidX

--- app/src/main/java/sammyt/jotnotes/LoginActivity.java
+++ app/src/main/java/sammyt/jotnotes/LoginActivity.java
@@ -1,6 +1,6 @@
 package sammyt.jotnotes;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
